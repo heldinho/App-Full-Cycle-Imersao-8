db597752517a3c027626a307ea8b7c4d10ba00b4
Helder Passos <helderferreirapassos@gmail.com>
2022-06-26 01:32:20 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import Card from "./src/components/Card";
+
+const aulas = [
+  {
+    key: 1,
+    title: `Full Cycle, Projeto prático e Microsserviços`,
+    image: require("./src/assets/1.png"),
+    date: "20/06",
+  },
+  {
+    key: 2,
+    title: `Nest.js e o Backend orientado a microsserviços`,
+    image: require("./src/assets/2.png"),
+    date: "21/06",
+  },
+  {
+    key: 3,
+    title: `React.js Tempo real com websockets e geolocalização`,
+    image: require("./src/assets/3.png"),
+    date: "22/06",
+  },
+  {
+    key: 4,
+    title: `Kafka Connect, Elasticsearch e Kibana: Integração e visualização de dados`,
+    image: require("./src/assets/4.png"),
+    date: "23/06",
+  },
+  {
+    key: 5,
+    title: `Kubernetes Deploy e escalabilidade de aplicações`,
+    image: require("./src/assets/5.png"),
+    date: "24/06",
+  },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        style={{ width: "100%" }}
+        contentContainerStyle={{ paddingVertical: 20 }}
+        showsVerticalScrollIndicator={false}
+      >
+        {aulas.map((item) => (
+          <Card {...item} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/Card.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+export default function Card(props) {
+  const { title, date, image } = props;
+  return (
+    <TouchableOpacity
+      style={{
+        elevation: 8,
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        width: "90%",
+        height: 150,
+        borderRadius: 8,
+        overflow: "hidden",
+        marginBottom: 20,
+        alignSelf: "center",
+      }}
+      activeOpacity={0.8}
+    >
+      <View>
+        <Image
+          source={image}
+          style={{ resizeMode: "contain", width: 150, height: 150 }}
+        />
+      </View>
+      <View
+        style={{ justifyContent: "space-between", padding: 20, width: "61.5%" }}
+      >
+        <View>
+          <Text style={{ color: "#444", fontSize: 16 }}>{title}</Text>
+        </View>
+        <View>
+          <View
+            style={{
+              borderBottomWidth: 1,
+              borderColor: "#ccc",
+              marginBottom: 10,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View>
+              <Text style={{ color: "#444" }}>Assistir</Text>
+            </View>
+            <View>
+              <Text style={{ color: "#444" }}>{date} NO AR</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
